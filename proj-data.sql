e0711a21ae1f19a67de1f4819882094ce9ee763f
Yyashwanth78 <mudeyashwanth20042003@gmail.com>
2023-04-06 22:56:56 +0530

    First commit

---
+++ proj-data.sql
@@ -0,0 +1,311 @@
+INSERT INTO user_info (user_id, name, mobile_no) 
+VALUES
+  ('10000','Holly Wise',9228395181),
+  ('10001','Jarrod Harrison',4438173864),
+  ('10002','Cassandra Branch',2413164311),
+  ('10003','Dennis Mcclure',3512589549),
+  ('10004','Abdul Blackburn',0358674108),
+  ('10005','Caleb Wilder',0576072628),
+  ('10006','Shay Whitney',7221626768),
+  ('10007','Nero Peck',3863441716),
+  ('10008','Leila Mathews',0983622165),
+  ('10009','Jessamine Ortiz',1353702782);
+
+INSERT INTO user_info (user_id, name, mobile_no) 
+VALUES
+  ('10010','Scott Vega',6728712713),
+  ('10011','Hamilton Serrano',0667857190),
+  ('10012','Randall Hubbard',0979858271),
+  ('10013','Dominic Barron',2404128176),
+  ('10014','Amanda Ortega',8850712252),
+  ('10015','Drake Thomas',5476812733),
+  ('10016','Wesley Kaufman',4660057215),
+  ('10017','Andrew Randolph',4526008743),
+  ('10018','Fuller Battle',3278615517),
+  ('10019','Amela Sullivan',1233823230);
+
+INSERT INTO user_info (user_id, name, mobile_no) 
+VALUES
+  ('10020','Zia Mccarthy',8181316018),
+  ('10021','Sybill Mendoza',3441094678),
+  ('10022','Flynn Frank',4494848665),
+  ('10023','Quyn Carter',5743419820),
+  ('10024','Rose Carlson',1629628620),
+  ('10025','Abel Craig',5631222135),
+  ('10026','Britanni Henderson',0266582442),
+  ('10027','Francesca Mosley',6212802429),
+  ('10028','Nayda Gross',8675551261),
+  ('10029','Aurora Levy',1432473018);
+
+INSERT INTO user_info (user_id, name, mobile_no) 
+VALUES
+  ('10030','Kevin Lewis',7639298113),
+  ('10031','Logan Gross',4374216065),
+  ('10032','Lareina Haney',2517143870),
+  ('10033','Kaitlin Warner',1334450566),
+  ('10034','Lunea Fuller',0548572073),
+  ('10035','Eric Burks',9761140585),
+  ('10036','Ray Holt',2574732897),
+  ('10037','Fletcher Lamb',1168967755),
+  ('10038','Erica Rice',6741845219),
+  ('10039','Pearl Madden',2648934443);
+
+INSERT INTO user_info (user_id, name, mobile_no) 
+VALUES
+  ('10040','Walker Ryan',8165373985),
+  ('10041','Sade Clarke',2345344408),
+  ('10042','Shea Cleveland',2160913769),
+  ('10043','Macaulay Mcneil',5168161426),
+  ('10044','Rashad Horton',2713921248),
+  ('10045','Cole Alvarado',0468470143),
+  ('10046','Kareem William',6436267063),
+  ('10047','Mufutau Huff',2335252026),
+  ('10048','Tamekah Morton',1767735247),
+  ('10049','Kadeem Kelley',0864222073);
+
+INSERT INTO user_info (user_id, name, mobile_no) 
+VALUES
+  ('10050','Russell Alvarado',8716968844),
+  ('10051','Amos Harrington',8484468915),
+  ('10052','Iris Scmidt',3678561948),
+  ('10053','Quyn Richards',2154351353),
+  ('10054','Dominic Davenport',4080744378),
+  ('10055','Ruth Taylor',4903462185),
+  ('10056','Kareem Owens',2185035903),
+  ('10057','Jelani Leblanc',7279487340),
+  ('10058','Yolanda Bean',5555419633),
+  ('10059','Noah Nash',6737422857);
+
+INSERT INTO user_info (user_id, name, mobile_no) 
+VALUES
+  ('10060','Gregory Levine',3383675016),
+  ('10061','Dominic Bryan',1374122665),
+  ('10062','Ifeoma Duncan',8980140143),
+  ('10063','Marny Phelps',8373646979),
+  ('10064','Nerea Boone',7455377593),
+  ('10065','Veda Bentley',2867528395),
+  ('10066','Karina Norton',1339841984),
+  ('10067','Brent Fulton',7324805368),
+  ('10068','Roanna Watson',2968663378),
+  ('10069','Shafira Gibson',8339735411);
+
+INSERT INTO user_info (user_id, name, mobile_no) 
+VALUES
+  ('10070','Xandra Petersen',3877964189),
+  ('10071','Hayley Rosario',8621563241),
+  ('10072','Jorden Nelson',2361523203),
+  ('10073','Harlan Meadows',4807337267),
+  ('10074','Jaden Cain',1651423252),
+  ('10075','Lee Kemp',7011876152),
+  ('10076','Axel Best',2283032402),
+  ('10077','Martina Clemons',3237256683),
+  ('10078','Adena Baker',6699216584),
+  ('10079','Rudyard Wilder',6457065689);
+
+INSERT INTO user_info (user_id, name, mobile_no) 
+VALUES
+  ('10080','Tiger Cameron',3940617410),
+  ('10081','August Patterson',8452584489),
+  ('10082','Carla Blankenship',7184292442),
+  ('10083','Brenda Castillo',3473621972),
+  ('10084','Barrett Cooper',1938312438),
+  ('10085','Kennan Chambers',1698230385),
+  ('10086','Roth Moon',8571336918),
+  ('10087','Armando Monroe',2774752422),
+  ('10088','Hayfa Willis',5429351478),
+  ('10089','Brynne Hendricks',1791077404);
+
+INSERT INTO user_info (user_id, name, mobile_no) 
+VALUES
+  ('10090','Helen Morales',8580971254),
+  ('10091','Breanna Stanley',6850762355),
+  ('10092','Quail Cohen',3195454407),
+  ('10093','Lucius Pitts',7512335363),
+  ('10094','Rinah Buchanan',4477536722),
+  ('10095','Jordan Barnes',2352571114),
+  ('10096','Juliet Todd',3892248845),
+  ('10097','Alvin Dickerson',6154755387),
+  ('10098','Hayes Ferrell',4210957351),
+  ('10099','Gretchen Adkins',1373343258);
+
+INSERT INTO user_password (user_id, password) 
+VALUES
+  ('10000','IEF10MLR1PL'),
+  ('10001','ILF41GYI9QB'),
+  ('10002','YPI30EIK7FW'),
+  ('10003','ILF45GNU9GK'),
+  ('10004','SBU88RQH2AS'),
+  ('10005','BGT76WLN7OX'),
+  ('10006','CJZ33BLT4HK'),
+  ('10007','GCQ53ODN7DC'),
+  ('10008','KUI73RKU1CO'),
+  ('10009','NIW79BLN4GT');
+
+INSERT INTO user_password (user_id, password) 
+VALUES
+  ('10010','NGX18YSM8CX'),
+  ('10011','CSB26TSO4VI'),
+  ('10012','FLY75DJY3KF'),
+  ('10013','JNY85WWQ2AG'),
+  ('10014','OTE08FKO4SQ'),
+  ('10015','FBJ24VSK6GX'),
+  ('10016','JMW16JSR4VT'),
+  ('10017','YWL95XYW0UG'),
+  ('10018','BFH87WYT4NG'),
+  ('10019','QHU73VVE2OU');
+
+INSERT INTO user_password (user_id, password) 
+VALUES
+  ('10020','EQN35SIW2BC'),
+  ('10021','LGY25NCH5EV'),
+  ('10022','NBR05HET0ON'),
+  ('10023','XRK15LWH5GI'),
+  ('10024','MRY10VVI8PD'),
+  ('10025','TJO16NJC1ML'),
+  ('10026','NIE47BVK2CH'),
+  ('10027','YLY81UHX1YX'),
+  ('10028','SPC52NHG7ND'),
+  ('10029','UNY71MKB5MK');
+
+INSERT INTO user_password (user_id, password) 
+VALUES
+  ('10030','DPU17PKO8VK'),
+  ('10031','WCB13YCW6WH'),
+  ('10032','LJN82NTR7SB'),
+  ('10033','BWU55DHS7PH'),
+  ('10034','ILL63LZH1SO'),
+  ('10035','EFY23FMS6EI'),
+  ('10036','VTG24DZJ8LD'),
+  ('10037','VIN18OLC0HP'),
+  ('10038','SVH78TFF3PX'),
+  ('10039','COK15HYV2RP');
+
+INSERT INTO user_password (user_id, password) 
+VALUES
+  ('10040','PLB32YEB8EC'),
+  ('10041','PSP05RSV7HR'),
+  ('10042','CFB74VVD3DJ'),
+  ('10043','JNQ17HHL6LD'),
+  ('10044','BDE46UAD2DR'),
+  ('10045','HUJ28MZB8ZB'),
+  ('10046','HEO61HPK8IH'),
+  ('10047','SWZ36JWL5MB'),
+  ('10048','URP87INF8ES'),
+  ('10049','BQH53PGG8FC');
+
+INSERT INTO user_password (user_id, password) 
+VALUES
+  ('10050','BUE74HRI0VF'),
+  ('10051','DUW15CEY2NN'),
+  ('10052','TQE95VAO3UB'),
+  ('10053','ZLG06WAJ3UL'),
+  ('10054','WVV56THP7NC'),
+  ('10055','QPP89SPH7QW'),
+  ('10056','RLH51DEB2VK'),
+  ('10057','EFE17CQH7HL'),
+  ('10058','RWP37NCQ5HB'),
+  ('10059','XZY15UAK1XZ');
+
+INSERT INTO user_password (user_id, password) 
+VALUES
+  ('10060','QKP85LFB4LG'),
+  ('10061','NGB73ZOR1QC'),
+  ('10062','YJP71EUO1QV'),
+  ('10063','ROO03PGB3RH'),
+  ('10064','ESL87WVR5FK'),
+  ('10065','HCN86NMT8RH'),
+  ('10066','YRU60KMD5SB'),
+  ('10067','QWT23UEE1KL'),
+  ('10068','EXJ84ZCT3RG'),
+  ('10069','TPP54KIH4CE');
+
+INSERT INTO user_password (user_id, password) 
+VALUES
+  ('10070','UDQ00HHV9UA'),
+  ('10071','DOU16GKD3EP'),
+  ('10072','OMY54OQG8EC'),
+  ('10073','RJZ77YQC4DH'),
+  ('10074','GPQ17PQQ6IL'),
+  ('10075','CMJ39NBB7RZ'),
+  ('10076','XBP04WDY8ZW'),
+  ('10077','VYO63XCF8ND'),
+  ('10078','RXA97AXT5XD'),
+  ('10079','BHF13BQR9KO');
+
+INSERT INTO user_password (user_id, password) 
+VALUES
+  ('10080','UDD16RMF7OI'),
+  ('10081','NII02KMW4MT'),
+  ('10082','DTU73UHY2DI'),
+  ('10083','EBI61DUC2YC'),
+  ('10084','DGS53PEG2VD'),
+  ('10085','CVJ78WYI2OA'),
+  ('10086','IJX05IAU2MV'),
+  ('10087','OWN69TBO9ET'),
+  ('10088','TEC19DHG1GS'),
+  ('10089','DRL93MII5SM');
+
+INSERT INTO user_password (user_id, password) 
+VALUES
+  ('10090','JGW21DNK4NS'),
+  ('10091','BCL12UNJ3NX'),
+  ('10092','TIB73JJF3TD'),
+  ('10093','OHJ27WSU1WZ'),
+  ('10094','ETH17YZU7YU'),
+  ('10095','CDS73UII4HY'),
+  ('10096','RYG56XVV8XX'),
+  ('10097','SKQ38XFM1DD'),
+  ('10098','JWC47QKO9MI'),
+  ('10099','KRX62EMG8DV');
+
+
+
+INSERT INTO aircraft(aircraft_id, aircraft_model, capacity, company) 
+VALUES
+('AA100', 'Boeing 737', 150, 'American Airlines'),
+('UA200', 'Airbus A320', 180, 'United Airlines'),
+('DL300', 'Boeing 777', 250, 'Delta Airlines'),
+('AC400', 'Embraer E190', 100, 'Air Canada'),
+('LH500', 'Airbus A350', 300, 'Lufthansa'),
+('BA600', 'Boeing 747', 400, 'British Airways'),
+('AF700', 'Airbus A330', 250, 'Air France'),
+('EK800', 'Boeing 787', 200, 'Emirates'),
+('SQ900', 'Airbus A380', 500, 'Singapore Airlines'),
+('TK100', 'Boeing 767', 220, 'Turkish Airlines'),
+('QF110', 'Airbus A321', 200, 'Qantas'),
+('JL120', 'Boeing 737 MAX', 170, 'Japan Airlines'),
+('KE130', 'Airbus A300', 250, 'Korean Air'),
+('MH140', 'Boeing 777X', 350, 'Malaysia Airlines'),
+('SU150', 'Airbus A340', 300, 'Aeroflot'),
+('OZ160', 'Boeing 777F', 300, 'Asiana Airlines'),
+('CX170', 'Airbus A319', 120, 'Cathay Pacific'),
+('NH180', 'Boeing 757', 180, 'All Nippon Airways'),
+('AI190', 'Airbus A310', 200, 'Air India'),
+('EY200', 'Boeing 747-8', 450, 'Etihad Airways'),
+('QR210', 'Airbus A330neo', 260, 'Qatar Airways'),
+('SA220', 'Boeing 737-800', 170, 'South African Airways'),
+('TG230', 'Airbus A220', 120, 'Thai Airways'),
+('NZ240', 'Boeing 787-10', 330, 'Air New Zealand'),
+('KL250', 'Airbus A321neo', 200, 'KLM'),
+('BR260', 'Boeing 777-300ER', 380, 'EVA Air'),
+('JJ270', 'Airbus A319neo', 140, 'LATAM Brasil'),
+('MU280', 'Boeing 737-700', 126, 'China Eastern Airlines'),
+('SK290', 'Airbus A350-1000', 440, 'SAS');
+
+INSERT INTO schedule(schedule_id, aircraft_id, departure_loc, departure_time, arrival_loc, arrival_time, at_capacity) 
+VALUES
+('SC001', 'AA100', 'JFK', '2023-04-07 12:00:00', 'LAX', '2023-04-07 14:30:00', false),
+('SC002', 'AA100', 'LAX', '2023-04-07 15:30:00', 'JFK', '2023-04-07 23:00:00', false),
+('SC003', 'UA200', 'ORD', '2023-04-07 11:00:00', 'SFO', '2023-04-07 14:00:00', false),
+('SC004', 'UA200', 'SFO', '2023-04-07 15:00:00', 'ORD', '2023-04-07 18:00:00', false),
+('SC005', 'DL300', 'ATL', '2023-04-07 09:00:00', 'LAX', '2023-04-07 12:30:00', false),
+('SC006', 'DL300', 'LAX', '2023-04-07 13:30:00', 'ATL', '2023-04-07 20:00:00', false),
+('SC007', 'AC400', 'YYZ', '2023-04-07 10:00:00', 'YVR', '2023-04-07 12:00:00', false),
+('SC008', 'AC400', 'YVR', '2023-04-07 13:00:00', 'YYZ', '2023-04-07 16:00:00', false),
+('SC009', 'LH500', 'FRA', '2023-04-07 08:00:00', 'JFK', '2023-04-07 11:00:00', false),
+('SC010', 'LH500', 'JFK', '2023-04-07 12:00:00', 'FRA', '2023-04-07 15:00:00', false),
+('SC011', 'BA600', 'LHR', '2023-04-07 10:00:00', 'JFK', '2023-04-07 13:00:00', false),
+('SC012', 'BA600', 'JFK', '2023-04-07 14:00:00', 'LHR', '2023-04-07 17:00:00', false),
+('SC013', 'AF700', 'CDG', '2023-04-07 09:00:00', 'LAX', '2023-04-07 12:30:00', false),
+('SC014', 'AF700', 'LAX', '2023-04-07 13:30:00', 'CDG', '2023-04-07 20:00:00', false),
+('SC015', 'EK800', 'DXB', '2023-04-07 06:00:00', 'JFK', '2023-04-07 11:00:00', false);
